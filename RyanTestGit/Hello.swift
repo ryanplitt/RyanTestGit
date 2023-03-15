dfb9fd61ffffed40c093edbcbfd5c8f412115713
Ryan Plitt <ryan.plitt@icloud.com>
2023-03-15 08:59:02 -0600

    Add new Hello file

---
+++ RyanTestGit/Hello.swift
@@ -0,0 +1,20 @@
+//
+//  Hello.swift
+//  RyanTestGit
+//
+//  Created by Ryan Plitt on 3/15/23.
+//
+
+import SwiftUI
+
+struct Hello: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Hello_Previews: PreviewProvider {
+    static var previews: some View {
+        Hello()
+    }
+}
