f0c35b7320e43778a810f4cf4517402343d17999
Ryan Plitt <ryan.plitt@icloud.com>
2023-03-15 08:50:05 -0600

    Initial Commit

---
+++ RyanTestGit/RyanTestGitApp.swift
@@ -0,0 +1,17 @@
+//
+//  RyanTestGitApp.swift
+//  RyanTestGit
+//
+//  Created by Ryan Plitt on 3/15/23.
+//
+
+import SwiftUI
+
+@main
+struct RyanTestGitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
